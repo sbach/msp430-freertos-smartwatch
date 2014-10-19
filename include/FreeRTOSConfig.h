27d278c82c10bf623ed0bae9564842ed041a8513
Stanislas Bach <stanislas.bach.heron@gmail.com>
2014-10-19 16:06:09 +0200

    Set the system frequency to 20MHz

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -82,7 +82,7 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( 25000000UL )
+#define configCPU_CLOCK_HZ				( 20000000UL )
 #define configLFXT_CLOCK_HZ       		( 32768L )
 #define configACLK_FREQ_HZ				( configLFXT_CLOCK_HZ )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
