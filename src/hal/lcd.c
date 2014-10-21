70d79a48b78d88240803c3e290e0d7f49fb0aa5b
StephaneD <stephane.dorre@gmail.com>
2014-10-21 20:39:16 +0200

    Add hal files to drive the Sharp memory LCD

---
+++ src/hal/lcd.c
@@ -0,0 +1,232 @@
+// vim: ft=c sw=4 ts=4 noet tw=90 cc=+1
+
+#include <msp430.h>
+#include <stdint.h>
+#include "lcd.h"
+
+
+
+
+uint8_t VCOMbit= 0x40;
+
+//use this flag to know if we have to Toggle the bit on VCOM to refresh the lcd
+uint8_t flagSendToggleVCOMCommand = 0;
+
+
+
+void lcd_init(void)
+{	
+	// Configure LCD_POWER_PIN as output to supply the LCD 
+	// and LCD_DISP_PIN as output for DISP
+	P3DIR |= BIT4 + BIT5;
+
+	// Provide power to LCD and turn on the lcd (via DISP pin)
+	P3OUT |= BIT4 + BIT5;
+	
+    // LCD_MOSI_PIN for SPI_MOSI mode
+	P3SEL |= BIT1;
+	
+	// LCD_SCLK_PIN for SPI_CLK mode
+	P3SEL |= BIT3;
+
+	// Configure LCD_SCS_PIN as output and clear pin
+	P3DIR |= BIT0;
+	
+	//Clear CS
+	P3OUT &= ~BIT0;
+
+    // config is on slau208n page 922
+	// Configure USCIB0 for SPI mode:
+	// SPI master,
+	// 3-pin SPI,
+	// 8-bit data,
+	// Data is captured on the first UCLK edge and changed on the following edge
+	// SMCLK sources USCI clock
+	//UCCKPH => clock phase select - data capture on first UCLK
+	// UCMSB => MSB first
+	// UCMST => master mode select
+	// UCSYNC => synchro mode enable (SPI is synchronous communication)
+	//set UCSWRST before init USCI register and 
+
+    UCB0CTL1 |= UCSWRST;
+
+	UCB0CTL0 |= UCCKPH + UCMSB + UCMST + UCSYNC;
+	UCB0CTL1 |= UCSSEL_3;
+	UCB0BR0 |= 0x08;
+	UCB0BR1 = 0;
+
+	// Initialize USCI state machine
+	UCB0CTL1 &= ~UCSWRST;
+}
+
+
+//*****************************************************************************
+//
+//! Flushes any cached drawing operations.
+//!
+//! \param pvDisplayData is a pointer to the driver-specific data for this
+//! display driver.
+//!
+//!
+//! This functions flushes any cached drawing operations to the display.  This
+//! is useful when a local frame buffer is used for drawing operations, and the
+//! flush would copy the local frame buffer to the display.
+//!
+//! \return None.
+//
+//*****************************************************************************
+void lcd_write(void *pvDisplayData)
+{
+	uint8_t *pucData = pvDisplayData;
+	int32_t xi =0;
+	int32_t xj = 0;
+
+	//image update mode(1X000000b)
+	uint8_t command = SHARP_LCD_CMD_WRITE_LINE;
+
+	//add VCOM inversion bit
+	command = command^VCOMbit;
+
+	//enable the communication on the bus
+	SetCS();
+
+	//send the order (write)
+	WriteCmdData(command);
+
+	//disable the toggle for the other routine. We just do it now
+	flagSendToggleVCOMCommand = SHARP_SKIP_TOGGLE_VCOM_COMMAND;
+
+	#ifdef LANDSCAPE
+	for(xj=0; xj<LCD_VERTICAL_MAX; xj++)
+	{
+		WriteCmdData(reverse(xj + 1));
+
+		for(xi=0; xi<(LCD_HORIZONTAL_MAX>>3); xi++)
+		{
+			WriteCmdData(*(pucData++));
+		}
+		WriteCmdData(SHARP_LCD_TRAILER_BYTE);
+	}
+#endif
+#ifdef LANDSCAPE_FLIP
+	pucData = &DisplayBuffer[LCD_VERTICAL_MAX-1][(LCD_HORIZONTAL_MAX>>3)-1];
+
+	for(xj=1; xj<=LCD_VERTICAL_MAX; xj++)
+	{
+		WriteCmdData(reverse(xj));
+
+		for(xi=0; xi < (LCD_HORIZONTAL_MAX>>3); xi++)
+		{
+			WriteCmdData(reverse(*pucData--));
+		}
+		WriteCmdData(SHARP_LCD_TRAILER_BYTE);
+	}
+#endif
+
+
+	WriteCmdData(SHARP_LCD_TRAILER_BYTE);
+
+	// Wait for last byte to be sent, then drop SCS
+	WaitUntilLcdWriteFinished();
+
+	// Ensure a 2us min delay to meet the LCD's thSCS
+	__delay_cycles(SYSTEM_CLOCK_SPEED * 0.000002);
+
+	//disable the communication bus
+	ClearCS();
+}
+
+
+//*****************************************************************************
+//
+//! Send command to clear screen.
+//!
+//! This function sets every pixel to the background color.
+//!
+//! \return None.
+//
+//*****************************************************************************
+void lcd_clearScreen(void)
+{
+	//clear screen mode(0X100000b)
+	uint8_t command = SHARP_LCD_CMD_CLEAR_SCREEN;
+	
+	//add VCOM inversion bit
+	command = command^VCOMbit;
+
+	//enable communication via SPI
+	SetCS();
+
+	//send the order (clear)
+	WriteCmdData(command);
+	
+	//disable the toggle VCOM operation for the other routine
+	flagSendToggleVCOMCommand = SHARP_SKIP_TOGGLE_VCOM_COMMAND;
+
+	WriteCmdData(SHARP_LCD_TRAILER_BYTE);
+
+	// Wait for last byte to be sent, then drop SCS
+	WaitUntilLcdWriteFinished();
+
+	// Ensure a 2us min delay to meet the LCD's thSCS
+	__delay_cycles(SYSTEM_CLOCK_SPEED * 0.000002);
+
+	//disable communication bus
+	ClearCS();
+}
+
+
+//*****************************************************************************
+//
+//! Send toggle VCOM command.
+//!
+//! This function toggles the state of VCOM which prevents a DC bias from being 
+//! built up within the panel.
+//!
+//! \return None.
+//
+//*****************************************************************************
+void lcd_sendToggleVCOMCommand(void)
+{
+	VCOMbit ^= SHARP_VCOM_TOGGLE_BIT;
+
+	if(SHARP_SEND_TOGGLE_VCOM_COMMAND == flagSendToggleVCOMCommand)
+	{
+		//clear screen mode(0X100000b)
+		uint8_t command = SHARP_LCD_CMD_CHANGE_VCOM;
+		//COM inversion bit
+		command = command^VCOMbit;
+
+	    SetCS();
+
+		WriteCmdData(command);
+		WriteCmdData(SHARP_LCD_TRAILER_BYTE);
+
+		// Wait for last byte to be sent, then drop SCS
+		WaitUntilLcdWriteFinished();
+
+		// Ensure a 2us min delay to meet the LCD's thSCS
+		__delay_cycles(SYSTEM_CLOCK_SPEED * 0.000002);
+
+		ClearCS();
+	}
+
+	flagSendToggleVCOMCommand = SHARP_SEND_TOGGLE_VCOM_COMMAND;
+}
+
+
+
+
+//! Reverses the bit order.- Since the bit reversal function is called
+//! frequently by the several driver function this function is implemented
+//! to maximize code execution
+
+const uint8_t referse_data[] = {0x0, 0x8, 0x4, 0xC, 0x2, 0xA, 0x6, 0xE, 0x1, 0x9, 0x5, 0xD, 0x3, 0xB, 0x7, 0xF};
+uint8_t reverse(uint8_t x)
+{
+  uint8_t b = 0;
+
+  b  = referse_data[x & 0xF]<<4;
+  b |= referse_data[(x & 0xF0)>>4];
+  return b;
+}
---
+++ src/hal/lcd.h
@@ -0,0 +1,136 @@
+// vim: ft=c sw=4 ts=4 noet tw=90 cc=+1
+
+ //*****************************************************************************
+//
+// HAL_MSP-EXP430_Sharp96x96 - Prototypes for the Sharp96x96 LCD display driver.
+//                                 There is no output from Sharp96x96 LCD.
+//
+//                 MSP430G2XX                    LCD Sharp96x96
+//                -----------------             -----------------
+//               |     UCB0CLK/P3.3|---------> |SCLK     EXTCOMIN|-- 0
+//            /|\|                 |           |            	 |
+//             | |    UCB0SIMO/P3.1|---------> |SI(MOSI)  EXTMODE|-- 0
+//             --|RST              |           |              	 |
+//               |     UCB0STE/P3.0|---------> |SCS              |
+//               |             	   |		   |         	     |
+//               |             P3.4|---------> |DISP             |
+//               |                 |		   |                 |
+//               |                 | 	       |                 |
+//               |                 |            -----------------
+//
+//*****************************************************************************
+
+#ifndef __HAL_LCD_H__
+#define __HAL_LCD_H__
+
+#include <stdint.h>
+
+//*****************************************************************************
+//
+// User Configuration for the LCD Driver
+//
+//*****************************************************************************
+
+// SYSTEM_CLOCK_SPEED (in Hz) allows to properly closeout SPI communication
+#define SYSTEM_CLOCK_SPEED      8000000
+
+// Define LCD Screen Orientation Here
+#define LANDSCAPE
+
+//Maximum Colors in an image color palette
+#define MAX_PALETTE_COLORS  2
+
+#define SHARP_BLACK				0x00
+#define SHARP_WHITE				0xFF
+
+#define SHARP_SEND_TOGGLE_VCOM_COMMAND		0x01
+#define SHARP_SKIP_TOGGLE_VCOM_COMMAND 		0x00
+
+#define SHARP_LCD_TRAILER_BYTE			0x00
+
+#define SHARP_VCOM_TOGGLE_BIT 		   	0x40
+
+#define SHARP_LCD_CMD_CHANGE_VCOM		0x00
+#define SHARP_LCD_CMD_CLEAR_SCREEN		0x20
+#define SHARP_LCD_CMD_WRITE_LINE		0x80
+
+// LCD Screen Dimensions
+#define LCD_VERTICAL_MAX                   96
+#define LCD_HORIZONTAL_MAX                 96
+
+//*****************************************************************************
+//
+// Clears CS line
+//
+// This macro allows to clear the Chip Select (CS) line
+//
+// \return None
+//*****************************************************************************
+
+#define ClearCS()                    	\
+       P3OUT &= ~BIT0;
+
+//*****************************************************************************
+//
+// Set CS line
+//
+// This macro allows to set the Chip Select (CS) line
+//
+// \return None
+//
+//*****************************************************************************
+
+#define SetCS()                    	\
+       P3OUT |= BIT0;
+
+//*****************************************************************************
+//
+// Waits until the SPI communication with the LCD is finished a command to
+// the LCD Driver
+//
+// \param None
+//
+// \return None
+//*****************************************************************************
+
+#define WaitUntilLcdWriteFinished()                \
+        while  (UCB0STAT & UCBUSY)
+
+//*****************************************************************************
+//
+// Writes command or data to the LCD Driver
+//
+// \param ucCmdData is the 8 or 16 bit command to send to the LCD driver
+// Uses the SET_LCD_DATA macro
+//
+// \return None
+//
+//*****************************************************************************
+
+#define WriteCmdData(ucCmdData)                    	\
+        do                                         	\
+        {                                           \
+            while (!(UCTXIFG & UCB0IFG));      		\
+            UCB0TXBUF = ucCmdData;                  \
+        }                                           \
+        while(0)
+
+
+
+//*****************************************************************************
+//
+// Prototypes for the globals exported by this driver.
+//
+//*****************************************************************************
+void lcd_init(void);
+
+void lcd_write(void *pvDisplayData);
+void lcd_clearScreen(void);
+void lcd_sendToggleVCOMCommand(void);
+
+
+uint8_t reverse(uint8_t x);
+
+
+#endif // __HAL_LCD_H__
+
