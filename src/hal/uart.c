e640ad7cd82fa940e6803d152b2578c5cd338ef7
Stanislas Bach <stanislas.bach.heron@gmail.com>
2014-11-01 16:15:08 +0100

    Switch to UCA1 (pins P5.6 and P5.7) * Fix documentation

--- src/hal/uart.c
+++ src/hal/uart.c
@@ -3,22 +3,22 @@
 
 
 /*******************************************************************************
- * \brief   Setup UCA3 UART interface
+ * \brief   Setup UCA1 UART interface
  *
  * \param void
  * \return void
  ******************************************************************************/
 void halDebugUARTInit( void )
 {
-    // Set P10.4 and P10.5 to UCA function
-    P10SEL |= BIT4;
-    P10SEL |= BIT5;
+    // Set P5.4 and P5.6 to UCA function
+    P5SEL |= BIT5;
+    P5SEL |= BIT6;
 
     // Put the USCI state machine in reset
-    UCA3CTL1 = UCSWRST;
+    UCA1CTL1 = UCSWRST;
 
     // Select the clock source
-    UCA3CTL1 |= UCSSEL__ACLK;
+    UCA1CTL1 |= UCSSEL__ACLK;
 
 
     // Configure the baud rate
@@ -31,26 +31,26 @@ void halDebugUARTInit( void )
     // Prescaler value UCBRx (16-bit)
     // @see page 913 of slau208n.pdf
     // BRx = (BR0 + BR1 × 256)
-    UCA3BR0 = 0x03;
-    UCA3BR1 = 0x00;
+    UCA1BR0 = 0x03;
+    UCA1BR1 = 0x00;
 
     // First and second modulation stage selection
     // These bits determine the modulation pattern.
     // @see page 913 of slau208n.pdf
-    UCA3MCTL = UCBRS_3 + UCBRF_0;
+    UCA1MCTL = UCBRS_3 + UCBRF_0;
 
 
     // Reset UCAxSTAT register to clear error flags
-    UCA3STAT = 0;
+    UCA1STAT = 0;
 
     // Initialize USCI state machine
-    UCA3CTL1 &= ~UCSWRST;
+    UCA1CTL1 &= ~UCSWRST;
 
-    // Enable USCI_A0 RX interrupt
-    UCA3IE = UCRXIE;
+    // Enable USCI_A1 RX interrupt
+    UCA1IE = UCRXIE;
 
     // Clear interrupt flags
-    UCA3IFG = 0;
+    UCA1IFG = 0;
 }
 
 /*******************************************************************************
@@ -65,23 +65,23 @@ void halDebugUARTWrite(const char *buf)
     while (*buf) WRITE_DEBUG_UART(*buf++);
 
     // Wait until transmit is done
-    while (UCA3STAT & UCBUSY);
+    while (UCA1STAT & UCBUSY);
 }
 
 /*******************************************************************************
- * \brief   ISR to handle events on the USCI_A3 pins.
+ * \brief   ISR to handle events on the USCI_A1 pins.
  *
  * \param void
  * \return void
  ******************************************************************************/
-void __attribute__ ( ( interrupt(USCI_A3_VECTOR) ) ) halDebugUARTISR( void )
+void __attribute__ ( ( interrupt(USCI_A1_VECTOR) ) ) halDebugUARTISR( void )
 {
-    switch (__even_in_range(UCA3IV,4)) {
+    switch (__even_in_range(UCA1IV,4)) {
         case UART_NO_INTERRUPT:
             break;
         case UART_RX_IFG:
-            UCA3IFG  &= ~UCRXIFG;
-            UCA3TXBUF = UCA3RXBUF;
+            UCA1IFG  &= ~UCRXIFG;
+            UCA1TXBUF = UCA1RXBUF;
             break;
         case UART_TX_IFG:
             break;
--- src/hal/uart.h
+++ src/hal/uart.h
@@ -12,7 +12,7 @@
 #define UART_RX_IFG         ( 2 )
 #define UART_TX_IFG         ( 4 )
 
-#define WRITE_DEBUG_UART(_x) {UCA3TXBUF = _x; while ((UCA3IFG & UCTXIFG) == 0);}
+#define WRITE_DEBUG_UART(_x) {UCA1TXBUF = _x; while ((UCA1IFG & UCTXIFG) == 0);}
 
 /*******************************************************************************
  * Prototypes
@@ -21,7 +21,7 @@
 void halDebugUARTInit( void );
 void halDebugUARTWrite(const char *buf);
 
-void __attribute__ ( ( interrupt(USCI_A3_VECTOR) ) ) halDebugUARTISR( void );
+void __attribute__ ( ( interrupt(USCI_A1_VECTOR) ) ) halDebugUARTISR( void );
 
 #endif /* HAL_UART_H */
 
